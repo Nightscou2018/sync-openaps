66f89865d2f17b57268103aace546565664cab66
fidoman <ericfeibelman@gmail.com>
2018-04-07 07:12:29 -0400

    working on multiple rigs

--- sync-openaps.sh
+++ sync-openaps.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
+#
+# Syncs openaps rigs preferences.json
+# All passwords must be the same and in the environment variable RSYNC_PASSWD
+#
+# You have to run rsync manually for the first time from this rig to the others to setup the 
+# initial trust key
+#
 
-sshpass -p $RSYNC_PASSWD rsync -rtuzv root@fido3:/root/test /root/
-sshpass -p $RSYNC_PASSWD  rsync -rtuzv /root/test root@fido3:/root/
+# user which is usually root
+user="root"
 
+# servers - list of all rig hostnames or ip addresses with spaces in between
+servers="eddie2 fido0"
+
+for server in $servers; do
+    echo 
+    echo syncing $server to $(hostname)
+    sshpass -p $RSYNC_PASSWD rsync -rtuzv ${user}@${server}:/root/test /root/
+    echo
+done
+
+echo
+echo 
+
+for server in $servers; do
+    echo 
+    echo syncing $(hostname) to $server
+    sshpass -p $RSYNC_PASSWD  rsync -rtuzv /root/test ${user}@${server}:/root/
+    echo
+done
