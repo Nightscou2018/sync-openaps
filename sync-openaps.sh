a19fb883a552bf8054b7d4caab7d74c137e563a7
fidoman <ericfeibelman@gmail.com>
2018-04-07 08:09:36 -0400

    updated to use string list variables for servers and files to be syncd

--- sync-openaps.sh
+++ sync-openaps.sh
@@ -6,14 +6,32 @@
 # You have to run rsync manually for the first time from this rig to the others to setup the 
 # initial trust key
 #
+epochdate=$(date +'%s')
 
 # user which is usually root
 user="root"
 
-# servers - list of all rig hostnames or ip addresses with spaces in between
-servers="eddie2 fido0"
+# servers - list of all rig hostnames or ip addresses - add your own list of rigs here 
+servers="
+        eddie2
+        fido0
+	"
 
-files="/root/myopenaps/preferences.json /root/.bash_profile"
+# servers - list of all files to sync - add your own list of filenames here 
+files="
+      /root/myopenaps/preferences.json
+      /root/.bash_profile
+      /etc/wpa_supplicant/wpa_supplicant.conf
+      "
+
+# save files just in case
+tar -czvf ./save-$epochdate.tar.gz $files
+
+for server in $servers; do
+  for file in $files; do
+    echo Debug $server -- $file
+  done
+done
 
 for server in $servers; do
   for file in $files; do
